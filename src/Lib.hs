36ec7339d3b531437fc49dbfd09d70c7c8c799a3
Seungheon Oh <seungheon.ooh@gmail.com>
2022-07-05 16:35:27 -0500

    shrinkers

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,23 +8,15 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeFamilyDependencies #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ViewPatterns #-}
 
 module Lib where
 
 import Data.ByteString (ByteString)
 import qualified Data.Text as T (intercalate, pack, unpack)
-import qualified GHC.Exts as Exts (IsList (fromList))
-import Plutarch (
-    PCon (pcon),
-    S,
-    Term,
-    compile,
-    plam,
-    pto,
-    (#),
-    (#$),
-    type (:-->),
- )
+import qualified GHC.Exts as Exts 
+import Plutarch
+import "liqwid-plutarch-extra" Plutarch.Extra.TermCont
 import Plutarch.Api.V1 (
     AmountGuarantees (NoGuarantees, NonZero, Positive),
     KeyGuarantees (Sorted, Unsorted),
@@ -51,8 +43,9 @@ import qualified Plutarch.Api.V1.AssocMap as Assoc (
     pfilter,
     pmap,
  )
+import Plutarch.Extra.Maybe
 import Plutarch.Api.V1.Time (PPOSIXTime (PPOSIXTime))
-import Plutarch.Api.V1.Tuple (pbuiltinPairFromTuple)
+import Plutarch.Api.V1.Tuple 
 import qualified "plutarch" Plutarch.Api.V1.Value as Value (
     pnormalize,
  )
@@ -69,43 +62,18 @@ import Plutarch.Evaluate (evalScript)
 import Plutarch.Extra.Map.Unsorted (psort)
 import Plutarch.Lift (
     PLift,
+    PUnsafeLiftDecl ,    
     PUnsafeLiftDecl (PLifted),
     pconstant,
     plift,
  )
 import Plutarch.List (PIsListLike, PList, PListLike (pcons, pnil))
-import Plutarch.Prelude (
-    PBool,
-    PByteString,
-    PEither (..),
-    PEq ((#==)),
-    PInteger,
-    PMaybe (..),
-    POrd (..),
-    PPair (..),
-    PRational,
-    PString,
-    PUnit,
-    Type,
-    pnot,
-    pshow,
-    ptraceError,
- )
-import Plutarch.Rational (PRational (PRational))
+import Plutarch.Prelude
+import Plutarch.Rational
 import Plutarch.Show (PShow)
 import Plutarch.Unsafe ()
-import Test.QuickCheck (
-    Arbitrary (arbitrary),
-    Gen,
-    Testable (property),
-    chooseInt,
-    elements,
-    sized,
-    vectorOf,
- )
-import Test.Tasty ()
-import Test.Tasty.Plutarch.Property ()
-import Test.Tasty.QuickCheck ()
+import Test.QuickCheck hiding (Sorted, NonZero, Positive)
+import Generics.SOP    
 
 {- | Finds Haskell level TestableTerm equivlance of Plutarch
      functions. This TypeFamily expects the input Plutarch functions to
@@ -135,13 +103,65 @@ instance {-# OVERLAPPING #-} FromPFunN a PBool where
 instance forall a b c d. (b ~ (c :--> d), FromPFunN c d) => FromPFunN a b where
     fromPFun f = \(TestableTerm x) -> fromPFun $ f # x
 
+{- | Extracts all @TestableTerm@s from give Plutarch function.
+
+ @since x.y.z
+-}
+type family PLamArgs (p :: S -> Type) :: [Type] where
+    PLamArgs (a :--> b) = TestableTerm a : (PLamArgs b)
+    PLamArgs _ = '[]
+
+{- | Make property by directly comparing behavior of Plutarch function
+     to Haskell counterpart.  This function will expect all Plutarch
+     types to be `plift`able and `pshow`able.  With given TestableTerm
+     generator, it will generate value for each arguments. Then, it
+     will apply generated value and lifted value to Plutarch and
+     haskell function. Once all arguments are applied, It will check
+     the equality of results.
+
+ @since x.y.z
+-}
+class (PLamArgs p ~ args) => HaskEquiv (h :: Type) (p :: S -> Type) args where
+    haskEquiv :: h -> TestableTerm p -> NP Gen args -> Property
+
+instance
+    forall ha hb pa pb hbArgs.
+    ( PLamArgs pb ~ hbArgs
+    , HaskEquiv hb pb hbArgs
+    , PLifted pa ~ ha
+    , PLift pa
+    , PShow pa
+    ) => HaskEquiv (ha -> hb) (pa :--> pb) (TestableTerm pa ': hbArgs) where
+    haskEquiv h (TestableTerm p) (g :* gs) =
+        forAll g $ \(TestableTerm x) -> haskEquiv (h $ plift x) (TestableTerm $ p # x) gs
+
+instance (PLamArgs p ~ '[], PLift p, PLifted p ~ h, Eq h) => HaskEquiv h p '[] where
+    haskEquiv h (TestableTerm p) _ = property $ plift p == h
+
+{- | Simplified version of `haskEquiv`. It will use arbitrary instead of
+     asking custom generators.
+
+ @since x.y.z
+-}
+haskEquiv' ::
+    forall h p args.
+    ( PLamArgs p ~ args
+    , HaskEquiv h p args
+    , All Arbitrary args
+    ) =>
+    h -> (forall s. Term s p) -> Property
+haskEquiv' h p = haskEquiv h (TestableTerm p) $ hcpure (Proxy @Arbitrary) arbitrary
+    
 -- | @since x.y.z
 instance Testable (TestableTerm PBool) where
     property (TestableTerm t) = property (plift t)
 
 {- | TestableTerm is a wrapper for closed Plutarch term. This
-   abstraction allows Plutarch values can be generated via QuickCheck
-   Generator.
+     abstraction allows Plutarch values can be generated via QuickCheck
+     Generator.
+
+     Hint: Typechecker is picky about how TestableTerm is constructed.
+     Meaning, TestableTerm throw error when it's composed.
 
  @since x.y.z
 -}
@@ -182,12 +202,18 @@ instance (forall (s :: S). Eq (Term s a)) => Eq (TestableTerm a) where
      @PShow@, `forAllShow` with custom show function is required to
      execute property check.
 
+     This only works with those that have PShow instance; it may cause
+     problems when dealing with Plutarch.Api datatypes which does not
+     have PShow instances.
+
  @since x.y.z
 -}
 instance PShow a => Show (TestableTerm a) where
     show (TestableTerm term) =
-        let (_, _, trace) = evalScript $ compile $ ptraceError (pshow term)
-         in T.unpack . T.intercalate " " $ trace
+        ""
+        -- TODO: Possibly use HasCallStack (?)
+        -- let (_, _, trace) = evalScript $ compile $ ptraceError (pshow term)
+        --  in T.unpack . T.intercalate " " $ trace
 
 {- | PArbitrary is Plutarch equivalent of `Arbitrary` typeclass from
      QuickCheck. It generates randomized closed term, which can be used
@@ -207,6 +233,10 @@ class PArbitrary (a :: S -> Type) where
     default parbitrary :: (PLift a, Arbitrary (PLifted a)) => Gen (TestableTerm a)
     parbitrary = (\x -> TestableTerm (pconstant x)) <$> arbitrary
 
+    pshrink :: TestableTerm a -> [TestableTerm a]
+    pshrink = const []
+
+
 {- | Any Plutarch type that implements `PArbitrary a` automatically gets
      `Arbitrary` of @TestableTerm a@. This is an interfacing between
      QuickCheck and Plutarch.
@@ -215,28 +245,44 @@ class PArbitrary (a :: S -> Type) where
 -}
 instance PArbitrary p => Arbitrary (TestableTerm p) where
     arbitrary = parbitrary
+    shrink = pshrink
+
+pdataT :: PIsData p => TestableTerm p -> TestableTerm (PAsData p)
+pdataT (TestableTerm x) = TestableTerm $ pdata x
+
+pfromDataT :: PIsData p => TestableTerm (PAsData p) -> TestableTerm p
+pfromDataT (TestableTerm x) = TestableTerm $ pfromData x
+
+pliftT :: (PLift p, PLifted p ~ h) => TestableTerm p -> h
+pliftT (TestableTerm x) = plift x
+
+pconstantT :: (PLift p, PLifted p ~ h) => h -> TestableTerm p
+pconstantT h = TestableTerm $ pconstant h
+
+pconT :: PCon p => (forall s. p s) -> TestableTerm p
+pconT p = TestableTerm $ pcon p
+
+pmatchT :: PMatch p => TestableTerm p -> (forall s. p s -> Term s b) -> TestableTerm b
+pmatchT (TestableTerm p) f = TestableTerm $ pmatch p f
 
 -- | @since x.y.z
 instance (PArbitrary p, PIsData p) => PArbitrary (PAsData p) where
     parbitrary = do
         (TestableTerm x) <- parbitrary
         return $ TestableTerm $ pdata x
+    pshrink = fmap pdataT . shrink . pfromDataT
 
 -- | @since x.y.z
-deriving instance PArbitrary PInteger
+instance PArbitrary PInteger where
+    pshrink = fmap pconstantT . shrink . pliftT
 
 -- | @since x.y.z
-deriving instance PArbitrary PBool
+instance PArbitrary PBool where
+    pshrink = fmap pconstantT . shrink . pliftT
 
 -- | @since x.y.z
-deriving instance PArbitrary PUnit
-
--- | @since x.y.z
-instance PArbitrary PByteString where
-    parbitrary = do
-        len <- chooseInt (0, 64)
-        bs <- genByteString len
-        return $ TestableTerm $ pconstant bs
+instance PArbitrary PUnit where
+    pshrink = fmap pconstantT . shrink . pliftT
 
 genByteStringUpto :: Int -> Gen ByteString
 genByteStringUpto m = sized go
@@ -247,6 +293,20 @@ genByteStringUpto m = sized go
 genByteString :: Int -> Gen ByteString
 genByteString l = Exts.fromList <$> vectorOf l arbitrary
 
+shrinkByteString :: ByteString -> [ByteString]
+shrinkByteString bs = do
+  xs' <- shrink . Exts.toList $ bs
+  pure . Exts.fromList $ xs'
+
+-- | @since x.y.z
+instance PArbitrary PByteString where
+    parbitrary = do
+        len <- chooseInt (0, 64)
+        bs <- genByteString len
+        return $ TestableTerm $ pconstant bs
+
+    pshrink = fmap pconstantT . shrinkByteString . pliftT
+
 -- | @since x.y.z
 instance PArbitrary PRational where
     parbitrary = do
@@ -254,15 +314,28 @@ instance PArbitrary PRational where
         (TestableTerm y) <- parbitrary
         return $ TestableTerm $ pcon $ PRational x y
 
+    pshrink (TestableTerm x) =
+        [ TestableTerm $ pcon $ PRational a b
+        | (TestableTerm a) <- shrink (TestableTerm $ pnumerator # x)
+        , (TestableTerm b) <- shrink (TestableTerm $ pdenominator # x)]
+
 -- | @since x.y.z
 instance PArbitrary PString where
     parbitrary = (\x -> TestableTerm (pconstant (T.pack x))) <$> arbitrary
+    pshrink = fmap (pconstantT . T.pack) . shrink . T.unpack . pliftT    
 
 -- | @since x.y.z
 instance PArbitrary a => PArbitrary (PMaybe a) where
     parbitrary = do
         (TestableTerm x) <- parbitrary
-        elements [TestableTerm $ pcon $ PJust x, TestableTerm $ pcon $ PNothing]
+        frequency [(3, return $ TestableTerm $ pcon $ PJust x), (1, return $ TestableTerm $ pcon $ PNothing)]
+    pshrink (TestableTerm x)
+        | plift (pisJust # x) =
+          (TestableTerm $ pcon PNothing) :
+          [ TestableTerm $ pcon $ PJust a
+          | (TestableTerm a) <- shrink (TestableTerm $ pfromJust # x)
+          ]
+        | otherwise = []
 
 -- | @since x.y.z
 instance (PIsData a, PArbitrary a) => PArbitrary (PMaybeData a) where
@@ -272,6 +345,13 @@ instance (PIsData a, PArbitrary a) => PArbitrary (PMaybeData a) where
             [ TestableTerm $ pcon $ PDJust $ pdcons @"_0" # (pdata x) # pdnil
             , TestableTerm $ pcon $ PDNothing pdnil
             ]
+    pshrink (TestableTerm x)
+        | plift $ pisDJust # x =
+          (TestableTerm $ pcon $ PDNothing pdnil) :
+          [ TestableTerm $ pcon $ PDJust $ pdcons @"_0" # (pdata a) # pdnil
+          | (TestableTerm a) <- shrink (TestableTerm $ pfromDJust # x)
+          ]
+        | otherwise = []            
 
 -- | @since x.y.z
 instance (PArbitrary a, PArbitrary b) => PArbitrary (PEither a b) where
@@ -279,6 +359,27 @@ instance (PArbitrary a, PArbitrary b) => PArbitrary (PEither a b) where
         (TestableTerm x) <- parbitrary
         (TestableTerm y) <- parbitrary
         elements [TestableTerm $ pcon $ PRight x, TestableTerm $ pcon $ PLeft y]
+
+    pshrink x
+        | pliftT $ isRight x =
+          [ pconT $ PRight a
+          | (TestableTerm a) <- shrink (pright x)
+          ]
+        | otherwise =
+          [ pconT $ PLeft a
+          | (TestableTerm a) <- shrink (pleft x)
+          ]          
+        where
+          isRight = flip pmatchT $ \case
+              PRight _ -> pconstant True
+              _ -> pconstant False
+          pright = flip pmatchT $ \case
+              PRight a -> a
+              _ -> ptraceError "asked for PRight when it is PLeft"
+          pleft = flip pmatchT $ \case
+              PLeft a -> a
+              _ -> ptraceError "asked for PLeft when it is PRight"
+        
 
 {- | Unfortunately, it is impossible to create @PBuiltinPair@ at the
      Plutarch level without getting into manipulating raw Plutus
@@ -299,6 +400,8 @@ instance
     where
     parbitrary = (\x -> TestableTerm (pconstant x)) <$> arbitrary
 
+    pshrink = fmap pconstantT . shrink . pliftT
+
 -- | @since x.y.z
 instance
     {-# OVERLAPPING #-}
@@ -313,6 +416,88 @@ instance
         (TestableTerm x) <- parbitrary
         return $ TestableTerm $ pfromData $ pbuiltinPairFromTuple (pdata x)
 
+    pshrink = fmap fromTuple . shrink . toTuple
+        where
+          toTuple = liftTestable (pfromData . ptupleFromBuiltin . pdata)
+          fromTuple = liftTestable (pfromData . pbuiltinPairFromTuple . pdata)    
+
+-- | @since x.y.z
+instance
+    ( PArbitrary a
+    , PArbitrary b
+    ) =>
+    PArbitrary (PPair a b)
+    where
+    parbitrary = do
+        (TestableTerm x) <- parbitrary
+        (TestableTerm y) <- parbitrary
+        return $ TestableTerm $ pcon $ PPair x y
+
+    pshrink x =
+        [ pconT $ PPair a b
+        | (TestableTerm a) <- shrink $ pfstT x
+        , (TestableTerm b) <- shrink $ psndT x
+        ]
+        where
+          pfstT = flip pmatchT $ \(PPair a _) -> a
+          psndT = flip pmatchT $ \(PPair _ a) -> a 
+        
+-- | @since x.y.z
+instance
+    ( PArbitrary a
+    , PArbitrary b
+    , PIsData a
+    , PIsData b
+    ) =>
+    PArbitrary (PTuple a b)
+    where
+    parbitrary = do
+        (TestableTerm x) <- parbitrary
+        (TestableTerm y) <- parbitrary
+        return $ TestableTerm $ ptuple # (pdata x) # (pdata y)
+
+    pshrink x = 
+        [ TestableTerm $ ptuple # a # b
+        | (TestableTerm a) <- shrink $ pfstT x
+        , (TestableTerm b) <- shrink $ psndT x
+        ]    
+        where
+          pfstT (TestableTerm x) = TestableTerm $ pfield @"_0" # x
+          psndT (TestableTerm x) = TestableTerm $ pfield @"_1" # x
+
+constrPList :: (PIsListLike b a) => [TestableTerm a] -> TestableTerm (b a)
+constrPList [] = TestableTerm $ pnil
+constrPList ((TestableTerm x) : xs) =
+    let (TestableTerm rest) = constrPList xs
+    in TestableTerm $ pcons # x # rest
+
+convToList :: (PIsListLike b a) => TestableTerm (b a) -> [TestableTerm a]
+convToList (TestableTerm x)
+    | not $ plift $ pnull # x = TestableTerm (phead # x) : convToList (TestableTerm $ ptail # x)
+    | otherwise = []
+
+genPListLike :: forall b a. (PArbitrary a, PIsListLike b a) => Gen (TestableTerm (b a))
+genPListLike = constrPList <$> arbitrary
+
+shrinkPListLike ::
+    forall a b.
+    ( PArbitrary a
+    , PIsListLike b a
+    ) =>
+    TestableTerm (b a) ->
+    [TestableTerm (b a)]
+shrinkPListLike xs' = constrPList <$> shrink (convToList xs')
+
+-- | @since x.y.z
+instance forall a. (PArbitrary a, PIsListLike PBuiltinList a) => PArbitrary (PBuiltinList a) where
+    parbitrary = genPListLike
+    pshrink = shrinkPListLike
+
+-- | @since x.y.z
+instance forall a. (PArbitrary a, PIsListLike PList a) => PArbitrary (PList a) where
+    parbitrary = genPListLike
+    pshrink = shrinkPListLike
+((    
 -- | @since x.y.z
 instance
     ( PArbitrary a
@@ -325,6 +510,10 @@ instance
     parbitrary = do
         (TestableTerm x) <- parbitrary
         return $ TestableTerm $ pcon $ PMap x
+
+    pshrink = fmap (\(TestableTerm x) -> pconT $ PMap x) . shrink . unMap
+        where
+          unMap = flip pmatchT $ \(PMap a) -> a
 
 -- | @since x.y.z
 instance
@@ -340,57 +529,19 @@ instance
         (TestableTerm x) <- parbitrary
         return $ TestableTerm $ psort # (pcon $ PMap x)
 
--- | @since x.y.z
-instance
-    ( PArbitrary a
-    , PArbitrary b
-    ) =>
-    PArbitrary (PPair a b)
-    where
-    parbitrary = do
-        (TestableTerm x) <- parbitrary
-        (TestableTerm y) <- parbitrary
-        return $ TestableTerm $ pcon $ PPair x y
-
--- | @since x.y.z
-instance
-    ( PArbitrary a
-    , PArbitrary b
-    , PIsData a
-    , PIsData b
-    ) =>
-    PArbitrary (PTuple a b)
-    where
-    parbitrary = do
-        (TestableTerm x) <- parbitrary
-        (TestableTerm y) <- parbitrary
-        return $ TestableTerm $ ptuple # (pdata x) # (pdata y)
-
-genPListLike :: forall b a. (PArbitrary a, PIsListLike b a) => Gen (TestableTerm (b a))
-genPListLike = sized $ \l -> do
-    len <- chooseInt (0, l)
-    xs <- vectorOf len parbitrary
-    return $ constrPList xs
-  where
-    constrPList :: [TestableTerm a] -> TestableTerm (b a)
-    constrPList [] = TestableTerm $ pnil
-    constrPList ((TestableTerm x) : xs) =
-        let (TestableTerm rest) = constrPList xs
-         in TestableTerm $ pcons # x # rest
-
--- | @since x.y.z
-instance forall a. (PArbitrary a, PIsListLike PBuiltinList a) => PArbitrary (PBuiltinList a) where
-    parbitrary = genPListLike
-
--- | @since x.y.z
-instance forall a. (PArbitrary a, PIsListLike PList a) => PArbitrary (PList a) where
-    parbitrary = genPListLike
+    pshrink = fmap (\(TestableTerm x) -> TestableTerm $ psort # (pcon $ PMap x)) . shrink . unMap
+        where
+          unMap = flip pmatchT $ \(PMap a) -> a        
 
 -- | @since x.y.z
 instance PArbitrary PPOSIXTime where
     parbitrary = do
         (TestableTerm x) <- parbitrary
         return $ TestableTerm $ pcon $ PPOSIXTime x
+
+    pshrink = fmap (\(TestableTerm x) -> pconT $ PPOSIXTime x) . shrink . unTime
+        where
+          unTime = flip pmatchT $ \(PPOSIXTime a) -> a
 
 -- | @since x.y.z
 instance (PIsData a, PArbitrary a) => PArbitrary (PExtended a) where
@@ -514,6 +665,10 @@ instance PArbitrary (PValue Unsorted NoGuarantees) where
         (TestableTerm val) <- parbitrary
         return $ TestableTerm $ pcon $ PValue val
 
+    pshrink = fmap (\(TestableTerm x) -> pconT $ PValue x) . shrink . unValue
+        where
+          unValue = flip pmatchT $ \(PValue a) -> a
+
 -- | @since x.y.z
 instance PArbitrary (PValue Unsorted NonZero) where
     parbitrary = do
@@ -523,6 +678,12 @@ instance PArbitrary (PValue Unsorted NonZero) where
                 pcon $
                     PValue $
                         Assoc.pmap # (plam $ \x -> Assoc.pfilter # (plam $ \y -> pnot # (y #== 0)) # x) # val
+
+    pshrink = fmap reValue . shrink . unValue
+        where
+          unValue = flip pmatchT $ \(PValue a) -> a
+          reValue (TestableTerm val) =
+              pconT $ PValue $ Assoc.pmap # (plam $ \x -> Assoc.pfilter # (plam $ \y -> pnot # (y #== 0)) # x) # val
 
 -- | @since x.y.z
 instance PArbitrary (PValue Unsorted Positive) where
@@ -534,11 +695,21 @@ instance PArbitrary (PValue Unsorted Positive) where
                     PValue $
                         Assoc.pmap # (plam $ \x -> Assoc.pfilter # (plam $ \y -> pnot # (0 #< y)) # x) # val
 
+    pshrink = fmap reValue . shrink . unValue
+        where
+          unValue = flip pmatchT $ \(PValue a) -> a
+          reValue (TestableTerm val) =
+              pconT $ PValue $ Assoc.pmap # (plam $ \x -> Assoc.pfilter # (plam $ \y -> pnot # (0 #< y)) # x) # val
+
 -- | @since x.y.z
 instance PArbitrary (PValue Sorted NoGuarantees) where
     parbitrary = do
         (TestableTerm val) <- parbitrary
         return $ TestableTerm $ pcon $ PValue val
+
+    pshrink = fmap (\(TestableTerm x) -> pconT $ PValue x) . shrink . unValue
+        where
+          unValue = flip pmatchT $ \(PValue a) -> a        
 
 -- | @since x.y.z
 instance PArbitrary (PValue Sorted NonZero) where
@@ -551,6 +722,12 @@ instance PArbitrary (PValue Sorted NonZero) where
                     $ PValue $
                         Assoc.pmap # (plam $ \x -> Assoc.pfilter # (plam $ \y -> pnot # (y #== 0)) # x) # val
 
+    pshrink = fmap reValue . shrink . unValue
+        where
+          unValue = flip pmatchT $ \(PValue a) -> a
+          reValue (TestableTerm val) =
+              pconT $ PValue $ Assoc.pmap # (plam $ \x -> Assoc.pfilter # (plam $ \y -> pnot # (y #== 0)) # x) # val                        
+
 -- | @since x.y.z
 instance PArbitrary (PValue Sorted Positive) where
     parbitrary = do
@@ -560,3 +737,9 @@ instance PArbitrary (PValue Sorted Positive) where
                 pcon $
                     PValue $
                         Assoc.pmap # (plam $ \x -> Assoc.pfilter # (plam $ \y -> (0 #<= y)) # x) # pto val
+
+    pshrink = fmap reValue . shrink . unValue
+        where
+          unValue = flip pmatchT $ \(PValue a) -> a
+          reValue (TestableTerm val) =
+              pconT $ PValue $ Assoc.pmap # (plam $ \x -> Assoc.pfilter # (plam $ \y -> pnot # (0 #< y)) # x) # val                        
--- test/Main.hs
+++ test/Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
-import Lib (FromPFunN (fromPFun))
-import Plutarch (Term, plam, (#), (#$), type (:-->))
+import Lib
+import Plutarch
+import Plutarch.Prelude
 import Plutarch.Api.V1 (
     AmountGuarantees (NoGuarantees, NonZero, Positive),
     KeyGuarantees (Sorted, Unsorted),
@@ -17,6 +19,7 @@ import "plutarch" Plutarch.Api.V1.Value qualified as Value (
     passertPositive,
     passertSorted,
  )
+import "liqwid-plutarch-extra" Plutarch.Extra.List    
 import Plutarch.Evaluate ()
 import Plutarch.Lift ()
 import Plutarch.Prelude (PBool, PEq (..), PInteger)
@@ -28,11 +31,16 @@ import Test.QuickCheck (
     Testable,
     forAllShow,
     resize,
+    forAll,
+    forAllShrink,
+    shrink,
  )
 import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.ExpectedFailure (expectFail)
 import Test.Tasty.Plutarch.Property ()
-import Test.Tasty.QuickCheck (testProperty)
+import Test.Tasty.QuickCheck (testProperty, Gen, withMaxSuccess )
+import "liqwid-plutarch-extra" Plutarch.Extra.List (preverse)
+import Generics.SOP
 
 selfEq :: PEq a => Term s (a :--> PBool)
 selfEq = plam $ \x -> x #== x
@@ -80,16 +88,58 @@ unsortedValueProp =
     unsortedValue = plam $ \x ->
         selfEq #$ Value.passertSorted # (punsafeCoerce x)
 
+addoneProp :: Property
+addoneProp =
+    haskEquiv (+1) (TestableTerm addone) (customGen :* Nil)
+    where
+      customGen :: Gen (TestableTerm PInteger)
+      customGen = do
+          n <- abs <$> arbitrary
+          return $ TestableTerm $ pconstant n
+      addone :: Term s (PInteger :--> PInteger)
+      addone = plam $ \x -> x + 1
+
+reverseProp :: Property
+reverseProp =
+    haskEquiv' (reverse @Integer) preverse
+
+testProp :: Property
+testProp = forAllShrink arbitrary shrink $ fromPFun test
+    where
+      test :: forall s. Term s (PList PInteger :--> PBool)
+      test = plam $ \x -> pnot #$ pelem # 5 # x #&& plength # x #== 3
+
+testProp3 :: Property
+testProp3 = forAllShrink arbitrary shrink $ fromPFun test
+    where
+      test :: forall s. Term s (PList PInteger :--> PBool)
+      test = plam $ \x -> pnot #$ pelem # 5 # x #&& plength # x #== 5 
+
+test2 :: Property
+test2 = forAllShrink arbitrary shrink $ test
+    where
+      test :: [Integer] -> Bool
+      test x = not $ elem 5 x && length x == 3
+
 main =
     defaultMain $
         testGroup "Tests" $
             [ testGroup "Values" $
-                [ testProperty "Generation of Sorted and Normalized Values" $ sortedValueProp
+                [ testProperty "Generation of Sorted and Normalized Values" $ withMaxSuccess 1000 $ sortedValueProp
                 , testProperty "Generation of Sorted, Normalized, and Positve Values" $ positiveSortedValueProp
                 , expectFail $ testProperty "Generation of Unsorted and Un-guaranteed Values" $ unsortedValueProp
                 ]
             , testGroup "Map" $
                 [ testProperty "Generation of Sorted PMap" $ sortedMapProp
                 , expectFail $ testProperty "Generation of Unsorted PMap" $ unsortedMapProp
+                ]
+            , testGroup "Some examples tests" $
+                [ testProperty "add one" $ addoneProp
+                , testProperty "add one" $ reverseProp
+                ]
+            , testGroup "Performance comp" $
+                [ expectFail $ testProperty "Plutarch w/ shrinker" $ withMaxSuccess 50000 $ testProp 
+                , expectFail $ testProperty "Plutarch w/ memory problem" $ withMaxSuccess 50000 $ testProp3
+                , expectFail $ testProperty "Haskell reference" $ withMaxSuccess 50000 $ test2   
                 ]
             ]
